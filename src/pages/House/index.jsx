cbc08dde49f2d15a3bdd1aa53aa0a67eb6bba57b
SimonGarbet <simon.garbet@gmail.com>
2023-05-29 16:35:33 +0200

    Réalisation de la page House

---
+++ src/pages/House/index.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import Error from '../../components/Error'
+
+import Collapse from '../../components/Collapse'
+import Carrousel from '../../components/Carrousel'
+import Rating from '../../components/Rating'
+import Tags from '../../components/Tags'
+
+import styles from '../../styles/house.module.css'
+
+function House() {
+  const [error, setError] = useState(false)
+  const [houseTarget, setHouseTarget] = useState([])
+
+  const { id } = useParams()
+
+  useEffect(() => {
+    async function fetchHouse() {
+      try {
+        const response = await fetch(`http://localhost:3000/datas.json`)
+        const houseTarget = await response.json()
+
+        setHouseTarget(houseTarget.filter((profile) => profile.id === id))
+
+        if (houseTarget[0].id === undefined) {
+          setError(true)
+        }
+      } catch (err) {
+        setError(true)
+      }
+    }
+    fetchHouse()
+  }, [])
+
+  if (error) {
+    return <Error />
+  }
+
+  console.log(houseTarget)
+  console.log(houseTarget[0]?.rating)
+
+  return (
+    <section>
+      {houseTarget.length === 0 ? (
+        <p>Chargement des données</p>
+      ) : (
+        <div className={styles.globalPage}>
+          <Carrousel pictures={houseTarget[0].pictures} />
+
+          <div className={styles.split}>
+            <div className={styles.locationGeneral}>
+              <h1>{houseTarget[0].title}</h1>
+              <p className={styles.locationGeneralText}>
+                {houseTarget[0].location}
+              </p>
+              <Tags text={houseTarget[0].tags} />
+            </div>
+
+            <div className={styles.locationProfile}>
+              <div className={styles.locationHost}>
+                <h2>{houseTarget[0].host.name}</h2>
+                <img
+                  src={houseTarget[0].host.picture}
+                  alt="Photographie de l'hôte"
+                ></img>
+              </div>
+              <div>
+                <Rating rate={houseTarget[0].rating} />
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.doubleCollapse}>
+            <Collapse title={'Description'} text={houseTarget[0].description} />
+            <Collapse title={'Equipements'} text={houseTarget[0].equipments} />
+          </div>
+        </div>
+      )}
+    </section>
+  )
+}
+
+export default House
