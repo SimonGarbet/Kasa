1db87b7e50471f37c986901affced67ece86ea03
SimonGarbet <simon.garbet@gmail.com>
2023-06-08 15:27:43 +0200

    Ajout logo SVG

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react'
 
 import styles from '../../styles/carrousel.module.css'
 
+import chevronRight from '../../assets/chevron-right-solid.svg'
+import chevronLeft from '../../assets/chevron-left-solid.svg'
+
 function Carrousel({ pictures }) {
   const [indexValue, setIndexValue] = useState(0)
 
@@ -29,14 +32,7 @@ function Carrousel({ pictures }) {
           }}
           className={styles.prevButton}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 320 512"
-          >
-            {' '}
-            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-          </svg>
+          <img src={chevronLeft} alt="Photographie précédente" />
         </button>
 
         <img
@@ -58,13 +54,7 @@ function Carrousel({ pictures }) {
           }}
           className={styles.nextButton}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 320 512"
-          >
-            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-          </svg>
+          <img src={chevronRight} alt="Photographie suivante" />
         </button>
       </div>
     )
--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react'
 
 import styles from '../../styles/collapse.module.css'
 
+import chevronUp from '../../assets/chevron-up-solid.svg'
+import chevronDown from '../../assets/chevron-down-solid.svg'
+
 function Collapse({ title, text }) {
   const [isOpen, setIsOpen] = useState(false)
 
@@ -12,13 +15,7 @@ function Collapse({ title, text }) {
       <div className={styles.container}>
         <div className={styles.titre} onClick={() => setIsOpen(false)}>
           <h2>{title}</h2>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 512 512"
-          >
-            <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
-          </svg>
+          <img src={chevronUp} alt="Logo chevron vers le haut" />
         </div>
         <div className={styles.textDescription}>
           {text.map((index) => (
@@ -30,13 +27,7 @@ function Collapse({ title, text }) {
       <div className={styles.container}>
         <div className={styles.titre} onClick={() => setIsOpen(true)}>
           <h2>{title}</h2>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 512 512"
-          >
-            <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-          </svg>
+          <img src={chevronDown} alt="Logo chevron vers le bas" />
         </div>
       </div>
     )
@@ -45,13 +36,7 @@ function Collapse({ title, text }) {
       <div className={styles.container}>
         <div className={styles.titre} onClick={() => setIsOpen(false)}>
           <h2>{title}</h2>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 512 512"
-          >
-            <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
-          </svg>
+          <img src={chevronUp} alt="Logo chevron vers le haut" />
         </div>
         <div className={styles.textDescription}>
           <p>{text}</p>
@@ -61,13 +46,7 @@ function Collapse({ title, text }) {
       <div className={styles.container}>
         <div className={styles.titre} onClick={() => setIsOpen(true)}>
           <h2>{title}</h2>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 512 512"
-          >
-            <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-          </svg>
+          <img src={chevronDown} alt="Logo chevron vers le bas" />
         </div>
       </div>
     )
